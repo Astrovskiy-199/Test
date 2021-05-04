be393cda18f5c910170e53eba035bdb64e53d7f0
astrovskiy-199 <astrovskiy.a@gmail.com>
2021-05-04 07:16:32 -0400

    super

--- Andrei_Astrouski_sa.sh
+++ Andrei_Astrouski_sa.sh
@@ -4,3 +4,6 @@ sed -i -e '7s/sbin/bin/g' -e '7s/nologin/bash/g' passwd_new
 awk -F [:] 'NR % 2 == 1' passwd_new > passwd_tmp && mv passwd_tmp passwd_new
 sed -i '/daemon/d' passwd_new
 awk -F [:] 'NR % 2 == 0  { gsub(/sbin/, "bin" ) gsub(/false/, "zsh") gsub(/nologin/, "zsh")   }1' passwd_new > passwd_tmp && mv passwd_tmp passwd_new
+awk '{gsub(/ee/,"alala" ) }1' poem_new.txt > poem_tmp.txt && mv poem_tmp.txt poem_new.txt
+awk '{gsub(/[/./]+/, "!") }1' poem_new.txt > poem_tmp.txt && mv poem_tmp.txt poem_new.txt
+sed -i '/./!d' poem_new.txt
